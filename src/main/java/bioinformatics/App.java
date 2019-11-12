7de9c2917cc7504aec1891b6852d657c46c841f9
virinchi <virinchivarmavannam@gmail.com>
2019-11-13 00:24:44 +0530

    Intial Commit for BioInformatics Project

---
+++ src/main/java/bioinformatics/App.java
@@ -0,0 +1,20 @@
+package bioinformatics;
+
+/**
+ *   Interface for bioInformatics App
+ *
+ *  __Frame shift mutation case__
+ *    index:   0  12  24 36
+ *     Ref:    A  B   C   D
+ *     Inp:    B  A   C   D
+ *    index:   12 0   24  36
+ *    add this case as frame shift mutation
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Work under progress!" );
+    }
+}
