1565e61a0bbc8b97cc74c576105fe592e0e9a814
PridhviChintala <56918012+PridhviChintala@users.noreply.github.com>
2020-05-30 00:25:48 +0530

    Updated reducer

--- gcptest/reducer.py
+++ gcptest/reducer.py
@@ -1,32 +1,5 @@
 #!/usr/bin/env python
 """reducer.py"""
-"""
-from operator import itemgetter
-import sys
-
-word_map = {}
-"""
-bad_chars = ['(',')', ',','\'']
-"""
-# input comes from STDIN
-for line in sys.stdin:
-    line = line.strip()
-    for ctch in bad_chars:
-        line = line.replace(ctch,'')
-    line = line.strip()
-    line = line.lower()
-    word_n_count = line.split()
-    count = int(word_n_count[1])
-    if word_n_count[0] in word_map.keys():
-        word_map[word_n_count[0]] += count
-    else:
-        word_map[word_n_count[0]] = count
-
-for wrd in word_map.keys():
-    print(wrd, word_map[wrd])
-"""
-
-import sys
 
 mut_map = {'Silent' : [],'Missense' : [] , 'Nonsense' : []}
 transition = []
@@ -36,12 +9,21 @@ transversion = []
 transit_fd = open("Transitions.txt",'w')
 transverse_fd = open("Transversions.txt",'w')
 mutations_fd = open("Mutations.txt",'w')
-
+bad_chars = ['(',')', ',','\'']
 for line in sys.stdin:
     line = line.strip()
-    #for ctch in bad_chars:
-     #   line = line.replace(ctch,'')
-    mut_type, tup = line.split()
+    for ctch in bad_chars:
+        line = line.replace(ctch,"")
+    splits = line.split("\t")
+
+    mut_type = splits[0].strip()
+    spits  = splits[1].strip()
+    spits = spits.split()
+    
+    spits[0] = int(spits[0])
+    #print(spits)
+    tup = tuple(spits)
+    #tup = (index,splits[2:])
     if mut_type == 'Transit' :
         transition.append(tup)
     elif mut_type == 'Transverse':
@@ -51,18 +33,18 @@ for line in sys.stdin:
 transition.sort()
 if len(transition) != 0:
     for i in transition:
-        transit_fd.write(i)
+        transit_fd.write(str(i))
         transit_fd.write("\n")
- else:
+else:
     transit_fd.write("No transitions found ")
 transit_fd.close() 
 
 transversion.sort()
 if len(transversion) != 0:
     for i in transversion:
-        transverse_fd.write(i)
+        transverse_fd.write(str(i))
         transverse_fd.write("\n")
- else:
+else:
     transverse_fd.write("No transitions found ")
 transverse_fd.close()
 
@@ -73,14 +55,6 @@ for i,j in mut_map.items():
     mutations_fd.write(i)
     mutations_fd.write(":\n")
     for tup in j:
-        mutations_fd.write(tup)
+        mutations_fd.write(str(tup))
         mutations_fd.write("\n")
 mutations_fd.close()
-        
-    
-
-
-        
-    
-
-    
